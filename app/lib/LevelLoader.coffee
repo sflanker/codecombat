sflanker/codecombat

commit 5d7687dc94c22586c9af78ab82a4a9b283ccdd25

Fix portraits for some ThangTypes
In PlayLevelView for GameDev levels, some thangs would have bad
portrait urls, for example Tree Stand 1 in Tabula Rasa. Make sure
thang types load the `raster` property in LevelLoader.

app/lib/LevelLoader.coffee
@@ -356,7 +356,7 @@ module.exports = class LevelLoader extends CocoClass
     if extantRequiredThangTypes.length < requiredThangTypes.length
       console.error "Some Thang had a blank required ThangType in components list:", components
     for thangType in extantRequiredThangTypes
-      url = "/db/thang.type/#{thangType}/version?project=name,components,original,rasterIcon,kind,prerenderedSpriteSheetData"
+      url = "/db/thang.type/#{thangType}/version?project=name,components,original,rasterIcon,raster,kind,prerenderedSpriteSheetData"
       @worldNecessities.push @maybeLoadURL(url, ThangType, 'thang')
 
   onThangNamesLoaded: (thangNames) ->

commit 342ef417f614f678059cf430af442f7643538a4f

:bug:Undo ThangType portrait fix
Caused game dev level alignment to be messed up.  Bug introduced by
5d7687dc94c22586c9af78ab82a4a9b283ccdd25

app/lib/LevelLoader.coffee
@@ -356,7 +356,7 @@ module.exports = class LevelLoader extends CocoClass
     if extantRequiredThangTypes.length < requiredThangTypes.length
       console.error "Some Thang had a blank required ThangType in components list:", components
     for thangType in extantRequiredThangTypes
-      url = "/db/thang.type/#{thangType}/version?project=name,components,original,rasterIcon,raster,kind,prerenderedSpriteSheetData"
+      url = "/db/thang.type/#{thangType}/version?project=name,components,original,rasterIcon,kind,prerenderedSpriteSheetData"
       @worldNecessities.push @maybeLoadURL(url, ThangType, 'thang')
 
   onThangNamesLoaded: (thangNames) ->

test/app/lib/LevelLoader.spec.coffee
@@ -143,7 +143,7 @@ describe 'LevelLoader', ->
     jasmine.Ajax.requests.sendResponses(responses)
     requests = jasmine.Ajax.requests.all()
     urls = (r.url for r in requests)
-    expect('/db/thang.type/mace/version?project=name,components,original,rasterIcon,raster,kind,prerenderedSpriteSheetData' in urls).toBeTruthy()
+    expect('/db/thang.type/mace/version?project=name,components,original,rasterIcon,kind,prerenderedSpriteSheetData' in urls).toBeTruthy()
 
   it 'loads components which are inherited by level thangs from thang type default components', ->
     new LevelLoader({supermodel:new SuperModel(), sessionID: 'id', levelID: 'id'})
@@ -167,7 +167,7 @@ describe 'LevelLoader', ->
     jasmine.Ajax.requests.sendResponses(responses)
     requests = jasmine.Ajax.requests.all()
     urls = (r.url for r in requests)
-    expect('/db/thang.type/wand/version?project=name,components,original,rasterIcon,raster,kind,prerenderedSpriteSheetData' in urls).toBeTruthy()
+    expect('/db/thang.type/wand/version?project=name,components,original,rasterIcon,kind,prerenderedSpriteSheetData' in urls).toBeTruthy()
 
   it 'loads components for item thang types which are inherited by level thangs from thang type default equips component configs', ->
     new LevelLoader({supermodel:new SuperModel(), sessionID: 'id', levelID: 'id'})
@@ -175,7 +175,7 @@ describe 'LevelLoader', ->
     responses =
       '/db/level/id': levelWithShaman
       '/db/thang.type/names': [thangTypeShamanWithWandEquipped]
-      '/db/thang.type/wand/version?project=name,components,original,rasterIcon,raster,kind': thangTypeWand
+      '/db/thang.type/wand/version?project=name,components,original,rasterIcon,kind': thangTypeWand
 
     jasmine.Ajax.requests.sendResponses(responses)
     requests = jasmine.Ajax.requests.all()